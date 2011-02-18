9441457e6a5e4a2a40b9d35fc361fc9bb8aec3bf
J Smith <jay@zoocasa.com>
2011-02-18 01:37:13 -0400

    Rails might not be defined.

--- lib/activerecord-postgresql-cursors.rb
+++ lib/activerecord-postgresql-cursors.rb
@@ -102,7 +102,7 @@ module ActiveRecord
   end
 end
 
-if Rails::VERSION::MAJOR >= 3
+if ActiveRecord::VERSION::MAJOR >= 3
   require File.join(File.dirname(__FILE__), 'postgresql_cursors_3')
 else
   require File.join(File.dirname(__FILE__), 'postgresql_cursors_2')
